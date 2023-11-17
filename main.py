67be9e7ca3828b2034ee971a9f7c281201c5a56e
dominominoes <dominic.t.jimenez@gmail.com>
2023-11-16 21:14:52 -0800

    Added word count funtion to python script

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     print(file_contents)
+
+def word_count():
+    number_of_words = 0
+    words = file_contents.split()
+    for word in words:
+        number_of_words += 1
+    return number_of_words
+
+print(f'There are {word_count()} words in this book.')
